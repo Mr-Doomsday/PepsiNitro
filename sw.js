775f2e19c8d945085370ccdd2ae76b807cb6c43d
Pepsi_Man <mr.doomsday07@gmail.com>
2024-03-07 13:38:32 +0000

    gvhh

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'version20'; // bump this version when you make changes.
+var CACHE_NAME = 'version21'; // bump this version when you make changes.
 // Put all your urls that you want to cache in this array
 var urlsToCache = [
     'index.html',
